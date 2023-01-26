46bf2ea8a02c75bdd69021ef01f0f42b0eb6662a
deodeepkunj <deodeepkunj@gmail.com>
2023-01-26 19:40:03 +0530

    validate boolesan type

--- src/components/Greet.tsx
+++ src/components/Greet.tsx
@@ -1,11 +1,17 @@
 type GreetProps = {
-    name: string,
-    messageCount: number,
-}
+  name: string;
+  messageCount: number;
+  isLoggedIn: boolean;
+};
 
-
-export const Greet  = (props: GreetProps) => {
-    return(
-        <h1>Welcome {props.name}! You have {props.messageCount} unread messages</h1>
-    )
-}
+export const Greet = (props: GreetProps) => {
+  return (
+    <>
+      {props.isLoggedIn ? (
+          <h1> Welcome {props.name}! You have {props.messageCount} unread messages </h1>
+      ) : (
+        <h1>Welcome Guest</h1>
+      )}
+    </>
+  );
+};
